682d19a2ed644cef3f81f3cb127b8931bbcdd761
manjuthaa <31506174+manjuthaa@users.noreply.github.com>
2018-01-23 11:16:53 +0000

    Create nearest even.c

---
+++ nearest even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i=2;
+	if(i%2==0)
+	{
+		printf("the num is even");
+	}
+	else
+	{
+		printf("the num is noteven ");
+			}
+
+	printf("%d",i+2);
+	return 0;
+}
+	
